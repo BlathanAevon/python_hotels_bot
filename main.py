abc5f80b21c3220a5f1d5f28ed53aa2d1c838eee
Федор Крысюк <bundemshake@gmail.com>
2021-09-18 10:49:52 +0300

    'commit'

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ maps_url = 'https://www.google.com/maps/dir//{},{}'
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage() -> Any:
     json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
+    update = types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
 
